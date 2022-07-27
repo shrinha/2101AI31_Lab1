4a62e2bbb77c72438089b77560bf989fc6b26052
shrinha <shreysinha2002@gmail.com>
2022-07-27 15:12:44 +0530

    Added code for Addition Operation

---
+++ calculator.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char op;
+    float a,b,res;
+    printf("Please Enter The Operation you want to perform(+,-,*,/):\n");
+    scanf("%c", &op);
+    printf("Enter the values of Operands :  \n ");
+   scanf("%f%f", &a, &b);
+
+    switch(op){
+      case '+': res = a + b;
+         break;
+
+      default: printf("\n Error  ");
+
+    }
+    printf("The value = %f", res);
+
+    return 0;
+}
